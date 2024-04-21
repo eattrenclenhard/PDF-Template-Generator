28da3cec8dd49724174d45102f02f74c67f41fb9
EatTrenClenHard <125723190+eattrenclenhard@users.noreply.github.com>
2024-04-22 01:00:47 +0800

    Day 24: Generate additional pages for each topic

--- main.py
+++ main.py
@@ -7,12 +7,17 @@ df = pd.read_csv('topics.csv')
 
 for index, row in df.iterrows():
     pdf.add_page()
-
     pdf.set_font(family='Times', style='BI', size=24)
     pdf.set_text_color(100, 100, 100)  # RGB
     pdf.cell(w=17, h=12, txt=row["Topic"],
              align='L', ln=1)
     pdf.line(x1=10, y1=21, x2=200, y2=21)
+    # spawns as many pages as stipulated in topics.csv
+    for page in range(row['Pages'] - 1):
+        print(f"Spawning page {page + 1} for {row['Topic']}")
+
+        pdf.add_page()
+
 
 # pdf.set_font(family='Times', style='U', size=10
 # pdf.cell(w=0, h=12, txt='Gutten tag!', align='L', ln=1, border=1)
