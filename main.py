85cf03748d89d0f34fa0551f9a7d446af98301eb
EatTrenClenHard <125723190+eattrenclenhard@users.noreply.github.com>
2024-04-23 02:17:56 +0800

    Day 24: Add footers

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf = FPDF(orientation='P', unit='mm', format='A4')
+pdf.set_auto_page_break(auto=False, margin=0)
+# necessary to push footer downwards without needing
+# to create multiple empty cells
 
 df = pd.read_csv('topics.csv')
 
@@ -9,14 +12,30 @@ for index, row in df.iterrows():
     pdf.add_page()
     pdf.set_font(family='Times', style='BI', size=24)
     pdf.set_text_color(100, 100, 100)  # RGB
-    pdf.cell(w=17, h=12, txt=row["Topic"],
-             align='L', ln=1)
+    pdf.cell(w=0, h=12, txt=row["Topic"],
+             align='L', ln=0)
+    # pdf.cell(w=17, h=12, txt=row["Topic"],
+    #          align='R', ln=1, border=1)
     pdf.line(x1=10, y1=21, x2=200, y2=21)
+
+    # footer
+    pdf.ln(265)  # in mm(the length of A4 paper is 297mm)
+    pdf.set_font(family='Times', style='I', size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, txt=row["Topic"],
+             align='R')
+
     # spawns as many pages as stipulated in topics.csv
     for page in range(row['Pages'] - 1):
         print(f"Spawning page {page + 1} for {row['Topic']}")
-
         pdf.add_page()
+
+        # footer
+        pdf.ln(277)  # in mm(the length of A4 paper is 297mm)
+        pdf.set_font(family='Times', style='I', size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=row["Topic"],
+                 align='R')
 
 
 # pdf.set_font(family='Times', style='U', size=10
